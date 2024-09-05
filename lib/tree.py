f7682feeae35092ccb57cf069d789b09314cfa64
Dakota <dakota.klasky@gmail.com>
2024-09-05 13:25:02 -0400

    tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,40 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+  
+    visit = [self.root]
+    while len(visit) > 0:
+      node = visit.pop()
+      if node['id'] == id:
+        return node
+      if len(node['children']) >0:
+        visit.insert(0,node['children'][0])
+    return None
+
+
+# tree = Tree(
+#   {
+#     'tag_name': 'body',
+#     'id': None,
+#     'children': [
+#       {
+#         'tag_name': 'div',
+#         'id': 'main',
+#         'children': [
+#           {
+#             'tag_name': 'h1',
+#             'id': 'heading',
+#             'text_content': 'Title',
+#             'children': []
+#           },
+#           {
+#             'tag_name': 'h2',
+#             'id': None,
+#             'text_content': 'Subitle',
+#             'children': []
+#           }
+#         ]
+#       }
+#     ]
+#   }
+# )
